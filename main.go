83aa13c0d852e0579a9bcb1914296bcf0fa85398
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2022-06-17 22:38:15 +0200

    Add simple createPerson function

--- main.go
+++ main.go
@@ -19,9 +19,21 @@ func getPersons(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, persons)
 }
 
+func createPerson(c *gin.Context) {
+	var newPerson person
+
+	if err := c.BindJSON(&newPerson); err != nil {
+		return
+	}
+
+	persons = append(persons, newPerson)
+	c.IndentedJSON(http.StatusCreated, newPerson)
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/persons", getPersons)
+	router.POST("/persons", createPerson)
 
     router.Run("localhost:8080")
 }
