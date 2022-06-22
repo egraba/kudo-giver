4bf90652724fa04667a7f63e8f5db81377f91ee6
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2022-06-22 22:51:22 +0200

    Add SQL queries for persons

---
+++ sql/create_persons_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE persons (
+	id INTEGER NOT NULL PRIMARY KEY,
+	first_name VARCHAR(32) NOT NULL,
+	last_name VARCHAR(32) NOT NULL,
+	birth_date DATE NOT NULL
+);
---
+++ sql/get_persons.sql
@@ -0,0 +1,2 @@
+SELECT * FROM persons
+ORDER BY first_name;
